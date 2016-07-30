210a18759c773e58602ea4c2f3487168d1cae28d
jbomyso1 <jbomyso1@binghamton.edu>
2016-07-30 19:22:36 -0400

    Added argument for input file name

---
+++ player.h
@@ -0,0 +1,17 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+
+struct player 
+{
+	std::string name;
+	unsigned int atBats;
+	unsigned int hits;
+	unsigned int doubles;
+	unsigned int triples;
+	unsigned int homeRuns;
+	unsigned int walks;
+	float runscreated;
+};
+
+#endif
